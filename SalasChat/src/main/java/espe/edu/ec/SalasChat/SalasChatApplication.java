dd72a495a49617dce154fedf7a5534207b9d809a
DylanGReinosoT <dgreinoso@espe.edu.ec>
2025-10-27 09:51:37 -0500

    Backend

---
+++ SalasChat/src/main/java/espe/edu/ec/SalasChat/SalasChatApplication.java
@@ -0,0 +1,13 @@
+package espe.edu.ec.SalasChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalasChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalasChatApplication.class, args);
+	}
+
+}
